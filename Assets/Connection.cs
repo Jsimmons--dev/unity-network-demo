c6a01b09b1a338ea30fe4a92daa95c1395383dd1
Josh Simmons <josh@phinsecurity.com>
2022-01-23 17:57:16 -0500

    begin adding acks infra to packets on both client and server

--- Assets/Connection.cs
+++ Assets/Connection.cs
@@ -4,104 +4,135 @@ using UnityEngine;
 using Newtonsoft.Json;
 using System.Net;
 using System;
+using System.Linq;
 using System.Text;
 using System.Net.Sockets;
 using System.Threading;
 
 public class NetworkInfo
 {
-	public string eventType { get; set; }
-	public string playerId { get; set; }
-	public string eventData { get; set; }
+    public string eventType { get; set; }
+    public string playerId { get; set; }
+    public string eventData { get; set; }
 }
 
 public class Connection : MonoBehaviour
 {
-	public GameObject playerPrefab;
-	public GameObject cameraPrefab;
-	private GameObject cameraInstance;
-	private string playerId;
-	private Dictionary<string, GameObject> map;
-	public Vector3 offset;
-	private ArrayList newEvents;
-	private UdpClient udpClient;
-	private Int32 connectionSequenceNumber = 1000;
-	private Thread thread1;
+    public GameObject playerPrefab;
+    public GameObject cameraPrefab;
+    private GameObject cameraInstance;
+    private string playerId;
+    private Dictionary<string, GameObject> map;
+    public Vector3 offset;
+    private ArrayList newEvents;
+    private UdpClient udpClient;
+    private Int32 connectionSequenceNumber = 1;
+    private Thread thread1;
 
-	private ArrayList mySyncedAL;
-	// Start is called before the first frame update
-
-	private void udpReciever()
-	{
-
-		while (true)
-		{
-			IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-
-			Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
-			string returnData = Encoding.ASCII.GetString(receiveBytes);
+    private ArrayList mySyncedAL;
+    // Start is called before the first frame update
 
 
-			Debug.Log(returnData);
+    private Int32 remoteSequenceNumber = 0;
 
-		}
-		// mySyncedAL.Add(returnData);
-		// Uses the IPEndPoint object to determine which of these two hosts responded.
-		// Console.WriteLine("This is the message you received " +
-		// 														 returnData.ToString());
-		// Console.WriteLine("This message was sent from " +
-		// 														RemoteIpEndPoint.Address.ToString() +
-		// 														" on their port number " +
-		// 														RemoteIpEndPoint.Port.ToString());
+    private Int32 ackBitField = 0;
 
-		// udpClient.Close();
-	}
+    private float waitTime = 1.0f / 2.0f;
+    private float timer = 0.0f;
+    private void udpReciever()
+    {
 
-	void Start()
-	{
-		udpClient = new UdpClient(0);
-		udpClient.Connect("localhost", 41234);
-		thread1 = new Thread(udpReciever);
-		thread1.Start();
-		// newEvents = new ArrayList();
-		// mySyncedAL = ArrayList.Synchronized( newEvents );
+        while (true)
+        {
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+            string returnData = Encoding.ASCII.GetString(receiveBytes);
+            // ArraySegment<Byte> remoteSeqArraySegment = new ArraySegment<Byte>(receiveBytes, 4, 4);
+
+            // Byte[] remoteSeq = remoteSeqArraySegment.Array;
+            Int32 previousRemoteSeq = remoteSequenceNumber;
+            remoteSequenceNumber = BitConverter.ToInt32(receiveBytes.Skip(4).Take(4).ToArray(), 0);
+            Int32 remoteSeqDiff = remoteSequenceNumber - previousRemoteSeq;
+
+            ackBitField = ackBitField << remoteSeqDiff;
+
+            Debug.Log(returnData);
+
+        }
+        // mySyncedAL.Add(returnData);
+        // Uses the IPEndPoint object to determine which of these two hosts responded.
+        // Console.WriteLine("This is the message you received " +
+        // 														 returnData.ToString());
+        // Console.WriteLine("This message was sent from " +
+        // 														RemoteIpEndPoint.Address.ToString() +
+        // 														" on their port number " +
+        // 														RemoteIpEndPoint.Port.ToString());
+
+        // udpClient.Close();
+    }
+
+    void Start()
+    {
+        udpClient = new UdpClient(0);
+        udpClient.Connect("localhost", 41234);
+        thread1 = new Thread(udpReciever);
+        thread1.Start();
+        // newEvents = new ArrayList();
+        // mySyncedAL = ArrayList.Synchronized( newEvents );
 
 
-	}
+    }
 
-	void Update()
-	{
+    void Update()
+    {
 
-		// 	foreach (string newEvent in mySyncedAL){
-		// 	Debug.Log(newEvent);
-		// }
+        // 	foreach (string newEvent in mySyncedAL){
+        // 	Debug.Log(newEvent);
+        // }
 
-		// mySyncedAL.Clear();
-		float xDirection = Input.GetAxis("Horizontal");
-		float zDirection = Input.GetAxis("Vertical");
+        // mySyncedAL.Clear();
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
 
-		NetworkInfo packet = new NetworkInfo();
-		packet.playerId = playerId;
-		packet.eventType = "MoveEvent";
-		packet.eventData = xDirection.ToString() + "," + zDirection.ToString();
-		string sendPacket = JsonConvert.SerializeObject(packet);
-		connectionSequenceNumber += 1;
-		Byte[] gameProtocol = Encoding.UTF8.GetBytes("5eee");
+        timer += Time.deltaTime;
 
-		Byte[] playerInfo = Encoding.UTF8.GetBytes(sendPacket);
+        if (timer > waitTime)
+        {
+            timer = timer - waitTime;
+            NetworkInfo packet = new NetworkInfo();
+            packet.playerId = playerId;
+            packet.eventType = "MoveEvent";
+            packet.eventData = xDirection.ToString() + "," + zDirection.ToString();
+            string sendPacket = JsonConvert.SerializeObject(packet);
+            connectionSequenceNumber += 1;
+            Byte[] gameProtocol = Encoding.UTF8.GetBytes("5eee");
 
-		Byte[] sequenceNumberToSend = BitConverter.GetBytes(connectionSequenceNumber);
+            Byte[] playerInfo = Encoding.UTF8.GetBytes(sendPacket);
 
-		if (BitConverter.IsLittleEndian)
-		{
-			Array.Reverse(sequenceNumberToSend);
-		}
+            Byte[] sequenceNumberToSend = BitConverter.GetBytes(connectionSequenceNumber);
 
-		Byte[] finalPacket = new Byte[gameProtocol.Length + sequenceNumberToSend.Length + playerInfo.Length];
-		Buffer.BlockCopy(gameProtocol, 0, finalPacket, 0, gameProtocol.Length);
-		Buffer.BlockCopy(sequenceNumberToSend, 0, finalPacket, gameProtocol.Length, sequenceNumberToSend.Length);
-		Buffer.BlockCopy(playerInfo, 0, finalPacket, gameProtocol.Length + sequenceNumberToSend.Length, playerInfo.Length);
+            Byte[] remoteSequenceNumberToSend = BitConverter.GetBytes(remoteSequenceNumber);
+            Byte[] ackBitFieldBytes = BitConverter.GetBytes(ackBitField);
 
-		udpClient.Send(finalPacket, finalPacket.Length);
-	}
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(sequenceNumberToSend);
+            }
+
+            List<Byte[]> stuffToSend = new List<Byte[]> { gameProtocol, sequenceNumberToSend, remoteSequenceNumberToSend };
+            Byte[] finalPacket = new Byte[16 + playerInfo.Length];
+            int byteCount = 0;
+
+
+            // System.Buffer.BlockCopy (System.Array src, System.Int32 srcOffset, System.Array dst, System.Int32 dstOffset, System.Int32 count) 
+            foreach (Byte[] stuff in stuffToSend)
+            {
+                Buffer.BlockCopy(stuff, 0, finalPacket, byteCount, stuff.Length);
+                byteCount += stuff.Length;
+            }
+
+            udpClient.Send(finalPacket, finalPacket.Length);
+        }
+    }
 }
--- multiplayer/index.js
+++ multiplayer/index.js
@@ -1,98 +1,194 @@
-// const { WebSocketServer } = require('ws')
-// const { v4: uuidv4 } = require('uuid')
-// const playerMap = {
-
-// }
-
-// const wss = new WebSocketServer({ port: 8080 });
-
-
-// wss.on('connection', function connection(ws, req) {
-
-//    ws.on('message', function message(data) {
-//       const message = JSON.parse(data)
-//       if (message.eventType == 'MoveEvent') {
-//          wss.clients.forEach(function each(client) {
-//             client.send(data.toString());
-//          });
-//       }
-//    })
-
-//    // if (!playerMap[req.socket.remoteAddress]) {
-//    //    playerMap[req.socket.remoteAddress] = uuidv4()
-//    // }
-//    // const playerId = playerMap[req.socket.remoteAddress]
-//    const playerId = uuidv4();
-//    console.log(`player: ${playerId} joined`)
-
-
-//    ws.send(JSON.stringify({ "playerId": playerId, "eventType": "self_connected", "eventData": "welcome to stuff" }))
-//    console.log(`sending other connected event for ${wss.clients.size} number of players`)
-//    wss.clients.forEach(function each(client) {
-//       if (client !== ws) {
-//          client.send(JSON.stringify({ playerId, eventType: "other_connected", eventData: "0,0" }));
-//       }
-//    });
-// })
-
 const dgram = require('dgram');
-const { connect } = require('http2');
-
 const server = dgram.createSocket('udp4')
+
+
+// example packet
+// 5eee 10 12 <00000000 00000000 00000000 00000000>
+// gameProtocol, localSequenceNumber, remoteSequenceNumber, ackBitField
+
+// <00000000 00000000 00000000 00000000> <00000000 00000000 00000000 00000000> <00000000 00000000 00000000 00000000> <00000000 00000000 00000000 00000000> ...
+
+
+
+
+
+// printing buffers as binary taken from 
+// https://stackoverflow.com/questions/66702935/how-do-i-print-a-node-buffer-as-binary
+// example usage console.log([...buffer].map(byteToBinaryString).join(" "));
+function byteToBinaryString(s) {
+   return s.toString(2).padStart(8, '0');
+}
+
+// This converts a 4 byte or larger buffer to a number assuming big endian
+//code taken from
+// https://stackoverflow.com/questions/14963182/how-to-convert-last-4-bytes-in-an-array-to-an-integer
+function convertBufferToNumber(buffer) {
+   var count = 0;
+   // assuming the buffer has at least four elements
+   for (var i = buffer.length - 4; i <= buffer.length - 1; i++) {
+      count = count << 8 + buffer[i];
+   }
+   return count
+}
+
+//buffer bitshifting code taken from a library that does it
+// https://github.com/cjdelisle/buffershift/blob/master/index.js
+
+//an easy way to shift a buffer left
+const shl = (buf /*:Buffer*/, shiftBits /*:number*/) => {
+   if (shiftBits < 0) { return shr(buf, -shiftBits); }
+   if (shiftBits !== (shiftBits | 0)) { throw new Error("shiftBits must be a 32 bit int"); }
+   const bytes = 2 * ((shiftBits >> 4) + ((shiftBits & 15) !== 0));
+   const bits = (bytes * 8) - shiftBits;
+   for (let reg = 0, i = 0; i - bytes < buf.length; i += 2) {
+      reg <<= 16;
+      if (i < buf.length - 1) {
+         reg |= buf.readUInt16BE(i);
+      } else if (i < buf.length) {
+         reg |= buf[i] << 8;
+      }
+      if (i - bytes >= 0) {
+         if (i - bytes < buf.length - 1) {
+            buf.writeUInt16BE((reg >>> bits) & 0xffff, i - bytes);
+         } else {
+            if (i - bytes !== buf.length - 1) { throw new Error(); }
+            buf[i - bytes] = reg >>> (bits + 8);
+         }
+      } else if (i - bytes === -1) {
+         buf[0] = reg >>> bits;
+      }
+   }
+};
+
+//an easy way to shift a buffer right
+const shr = (buf /*:Buffer*/, shiftBits /*:number*/) => {
+   if (shiftBits < 0) { return shl(buf, -shiftBits); }
+   if (shiftBits !== (shiftBits | 0)) { throw new Error("shiftBits must be a 32 bit int"); }
+   const bytes = 2 * ((shiftBits >> 4) + ((shiftBits & 15) !== 0));
+   const bits = 16 - ((bytes * 8) - shiftBits);
+   for (let reg = 0, i = buf.length - 2; i + bytes >= -1; i -= 2) {
+      reg >>>= 16;
+      if (i >= 0) {
+         reg |= buf.readUInt16BE(i) << 16;
+      } else if (i === -1) {
+         reg |= buf[0] << 16;
+      }
+      if (i + bytes + 1 < buf.length) {
+         if (i + bytes >= 0) {
+            buf.writeUInt16BE((reg >>> bits) & 0xffff, i + bytes);
+         } else {
+            if (i + bytes + 1) { throw new Error(); }
+            buf[0] = reg >>> bits;
+         }
+      } else if (i + bytes + 1 === buf.length) {
+         buf[i + bytes] = reg >>> (bits + 8);
+      }
+   }
+};
 
 server.on('error', (err) => {
    console.log(`server error:\n${err.stack}`);
    server.close();
 });
 
+// This is used to tell who is connected and how long it has been since there has been a message
 const connectedPlayers = new Map()
+// This keeps track of our local sequence number. That is, for every message we send, we increment add this
+// This helps the game know what packets have been received and which have been dropped
 const connectedPlayersSeq = new Map()
 
+//This keeps track of the remote sequence number of the most recently seen packet from a connected player
+const connectedPlayersRemoteSeq = new Map()
+
+//This keeps track of the 32 previous packets received from a connected player
+// This gives us redundancy in what packets we've seen.
+// If a clients sends 30 packets per second and we're sending 10 back then 
+// We would have to have a packet delayed for an entire second to actually miss it
+const connectedPlayersAckBitfield = new Map()
+
+//We increment the inactivity timer for a connected player using a dedicated async loop
 const checkForDisconnection =
    setInterval(() => {
+      // walk through each of the connected players
       for (let [playerId, playerTimeoutCounter] of connectedPlayers.entries()) {
+         // increment how long they've been connected (this gets reset on message received)
          connectedPlayers.set(playerId, playerTimeoutCounter + 1)
+         // if it's been longer than 10 seconds since we've received a message from this player
          if (playerTimeoutCounter > 10) {
             console.log(`player ${playerId} disconnected`)
+            //remove them from the connected players
             connectedPlayers.delete(playerId)
+            //remove their sequence number counter
             connectedPlayersSeq.delete(playerId)
          }
       }
    }, 1000)
+
+// This is the arbitrary protocol id that our client and server include so that it is known
+// if packets are from the client / server for this game.
 const gameProtocol = Buffer.from("5eee")
+
 server.on('message', (msg, rinfo) => {
 
-   console.log(msg)
+   // Only use this packet if it matches our game protocol
    if (msg.slice(0, 4).compare(gameProtocol) === 0) {
-      // console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-      if (!connectedPlayers.has(`${rinfo.address}:${rinfo.port}`)) {
-         console.log(`player ${rinfo.address}:${rinfo.port} connected`)
-         connectedPlayersSeq.set(`${rinfo.address}:${rinfo.port}`, Buffer.alloc(4))
-      }
-      connectedPlayers.set(`${rinfo.address}:${rinfo.port}`, 0)
+      const playerId = `${rinfo.address}:${rinfo.port}`
+      // If this player currently is not connected
+      if (!connectedPlayers.has(playerId)) {
+         console.log(`player ${playerId}} connected`)
+         // Save them into the connected players map
+         connectedPlayersSeq.set(playerId, Buffer.alloc(4))
+         connectedPlayersRemoteSeq.set(playerId, Buffer.alloc(4))
+         connectedPlayersAckBitfield.set(playerId, Buffer.alloc(4))
+      } 
+      //parse the remote sequence number and store it so we can ack TODO
+      const previousRemoteSeq = connectedPlayersRemoteSeq.get(playerId)
+      connectedPlayersRemoteSeq.set(playerId, msg.slice(4, 8))
+      const remoteSeqDiff = convertBufferToNumber(msg.slice(4, 8)) - convertBufferToNumber(previousRemoteSeq)
+      const oldBitField = connectedPlayersAckBitfield.get(playerId)
+      // console.log(remoteSeqDiff)
+      // note that this is being done in place so no need to set again in the map
+      shl(oldBitField,  remoteSeqDiff)
+      //reset the player's inactivity timer
+      connectedPlayers.set(playerId, 0)
    }
 });
 
+//Any incrementing numbers are stored using fixed length buffers in our packets
+// it is difficult to use JS since all the types are handled by the engine.
+// This method allows us to increment (and wrap around?) our packet numbres
 function incrementBE(buffer) {
    for (var i = buffer.length - 1; i >= 0; i--) {
       if (buffer[i]++ !== 255) break;
    }
 }
 
+
+// This is where we communicate back to connected clients. We try to send 10 packets per
+// second to each connected player regardless of whether or not we have something to send right now.
+// That is useful based on how we calculate if someone is connected or not.
 const sendDataToClients =
    setInterval(() => {
       for (let [playerId, playerTimeoutCounter] of connectedPlayers.entries()) {
-         const buf1 = Buffer.from('5eee');
-         const buf2 = connectedPlayersSeq.get(playerId)
-         connectedPlayersSeq.set(incrementBE(connectedPlayersSeq.get(playerId)))
+         const gameProtocol = Buffer.from('5eee');
+         //Increment the local sequence number for this packet
+         // This allows the client to ack it back to us when they get it.
+         incrementBE(connectedPlayersSeq.get(playerId))
+         const localSequenceNumber = connectedPlayersSeq.get(playerId)
+         const ackNumber = connectedPlayersRemoteSeq.get(playerId)
+         const ackBitField = connectedPlayersAckBitfield.get(playerId)
          const [playerAddress, playerPort] = playerId.split(':')
-         server.send([buf1, buf2], playerPort, playerAddress, (err) => {
+         console.log(`${[...connectedPlayersSeq].map(byteToBinaryString).join(" ")} --- local seq number`)
+         console.log(`${[...ackNumber].map(byteToBinaryString).join(" ")} --- remote seq number`)
+         console.log(`${[...ackBitField].map(byteToBinaryString).join(" ")} --- ack bitfield`)
+         console.log()
+         server.send([gameProtocol, localSequenceNumber, ackNumber, ackBitField], playerPort, playerAddress, (err) => {
             if (err) {
                console.log(err)
             }
          });
       }
-   }, 100)
+   }, 500)
 
 
 server.on('listening', () => {
